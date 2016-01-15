c8ca0ef53e46969d7e26a2dc22ed03abe317f6ba
rsgc_edwards_g <gedwards@rsgc.on.ca>
2016-01-15 17:03:41 +0000

    Added php that allows users to log in to their accounts. Php was designed to distinguish between teachers and students based on the username they are using. Resolves #13.

--- index.php
+++ index.php
@@ -1,6 +1,60 @@
 <?php
+    if(isset($_POST['submit']))  {
 
+    // DB Connection Parameters
 
+    $host = "209.236.71.62";
+    $user = "mrgogor3_PRJXUSR";
+    $pass = "query370?Dinah";
+    $db = "mrgogor3_PRJX";
+    $port = 3306;
+    
+    // Establish the connection with database user names and passwords
+    $connection = mysqli_connect($host, $user, $pass, $db, $port) or die(mysql_error());
+    
+    // Process a log in
+    $provided_username = htmlspecialchars($_POST['username']);
+    $provided_password = htmlspecialchars($_POST['password']);
+    $query = "SELECT password FROM teacher WHERE username = ('" . $provided_username . "');";
+    
+    // Get results
+    $result = mysqli_query($connection, $query);
+    
+    // if there are no users in the teacher table, try the student table
+    if (mysqli_num_rows($result) == 0) {
+        $query = "SELECT password FROM students WHERE username = ('" . $provided_username . "');";
+        // Get the results again
+        $result = mysqli_query($connection, $query);   
+    }
+    
+    // Compare the provided password to the stored password
+    if ($result == false) {
+        $message['general'] = "An unexpected error occurred. Please try again later.";
+    } else {
+        if (mysqli_num_rows($result) == 0) {
+          $message['general'] = "Error. The user <strong>" . $provided_username . "</strong> was not found.";
+        } else {
+          // We have a result, now do the comparison of passwords
+          $row = mysqli_fetch_assoc($result);
+          $stored_password = $row['password'];
+          if (password_verify($provided_password, $stored_password) == true) {
+                // All is well, set the session
+                session_start();
+                $_SESSION['username'] = $provided_username; 
+                
+                // Now re-direct to the logged-in home page
+                $host  = $_SERVER['HTTP_HOST'];
+                $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+                $extra = 'home.php';
+                header("Location: http://$host$uri/$extra");
+                exit;
+          } else {
+              $message['general'] = "Incorrect password for user <strong>" . $provided_username . "</strong>.";
+          }
+        }
+    }
+    
+}
 
 
 ?>
