9af62912d07e62c8070fc47051e21f9f8a497e75
rsgc_edwards_g <gedwards@rsgc.on.ca>
2016-01-25 17:35:16 +0000

    Fixed problem with the teacher login page so the username isn't referred to as the ID. Users are now recognizeable.

--- indexTeacher.php
+++ indexTeacher.php
@@ -69,7 +69,7 @@
     <main>
         <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
             User ID:<br/>
-            <input type="text" name="ID" value="<?php echo $_POST['ID'] ?>" maxlength="20" size="20"> <?php echo $message['ID']; ?><br/><br/>
+            <input type="text" name="username" value="<?php echo $_POST['username'] ?>" maxlength="20" size="20"> <?php echo $message['username']; ?><br/><br/>
             Password:<br/>
             <input type="password" name="password" value="<?php echo $_POST['password'] ?>" maxlength="20" size="20"> <?php echo $message['password']; ?><br/><br/>
             <input type="submit" name="submit" value="Login">
