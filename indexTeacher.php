16b5cfb53f4ee053cadb8ecb953e2fe473f57f00
rsgc_dunphy_j <jdunphy@rsgc.on.ca>
2016-03-01 16:39:19 +0000

    removed the buggy header

--- indexTeacher.php
+++ indexTeacher.php
@@ -1,4 +1,5 @@
 <?php
+    session_start();
     if(isset($_POST['submit']))  {
 
     // DB Connection Parameters
@@ -66,7 +67,7 @@
 <body>
 
     <header>
-      //to be filled with a shmee graphic
+        Shmee
     </header>
 
     <main>
