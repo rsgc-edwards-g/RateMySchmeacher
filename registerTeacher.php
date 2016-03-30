45bb29514f2b5de5cf75e97b44c297bacf832105
rsgc_edwards_g <gedwards@rsgc.on.ca>
2016-03-30 16:36:13 +0000

    Changed the teacher register page so that teachers can activate their accounts by entering their first and last names and desired usernames and passwords. Resolves #39.

--- registerTeacher.php
+++ registerTeacher.php
@@ -7,7 +7,6 @@ if(isset($_POST['submit']))  {
     $provided_username = htmlspecialchars(trim($_POST['username']));
     $provided_first_name = htmlspecialchars(trim($_POST['first_name']));
     $provided_last_name = htmlspecialchars(trim($_POST['last_name']));
-    $provided_email = htmlspecialchars(trim($_POST['email']));
     $provided_password = htmlspecialchars(trim($_POST['password']));
 
     // Verify that user_id, name, email, and password were provided.
@@ -19,9 +18,6 @@ if(isset($_POST['submit']))  {
     }
     if (strlen($provided_last_name) == 0) {
         $message['last_name'] = "Last name is required.";
-    }
-    if (strlen($provided_email) == 0) {
-        $message['email'] = "Email is required.";
     }
     if (strlen($provided_password) == 0) {
         $message['password'] = "A password is required.";
@@ -48,7 +44,7 @@ if(isset($_POST['submit']))  {
         if (! $row = mysqli_fetch_assoc($result) ) {
             // Proceed with creating a user based on provided username
             $hashed_password = password_hash($provided_password, PASSWORD_DEFAULT);
-            $query = "INSERT INTO teacher (first_name, last_name, username, password, email) VALUES ('" . $provided_first_name . "', '" . $provided_last_name . "', '" . $provided_username . "', '" . $hashed_password . "', '" . $provided_email . "')";
+            $query = "UPDATE teacher SET username='" . $provided_username ."', password='" . $hashed_password . "' WHERE first_name = '" . $provided_first_name . "' AND last_name = '" . $provided_last_name . "';";
 
             // Check to see if query succeeded
             if (! mysqli_query($connection, $query)) {
@@ -91,12 +87,10 @@ if(isset($_POST['submit']))  {
             <input type="text" name="last_name" value="<?php echo $_POST['last_name'] ?>" maxlength="45" size="45"> <?php echo $message['last_name']; ?><br/><br/>
             Create a User ID:<br/>
             <input type="text" name="username" value="<?php echo $_POST['username'] ?>" maxlength="45" size="45"> <?php echo $message['username']; ?><br/><br/>
-            Enter your email:<br/>
-            <input type="text" name="email" value="<?php echo $_POST['email'] ?>" maxlength="45" size="45"> <?php echo $message['email']; ?><br/><br/>
             Enter a password:<br/>
             <input type="password" name="password" value="<?php echo $_POST['password'] ?>" maxlength="45" size="45"> <?php echo $message['password']; ?><br/><br/>
             
-            <input type="submit" name="submit" value="Create new account"><br/><br/>
+            <input type="submit" name="submit" value="Activate account"><br/><br/>
             
             <?php echo $message['general']; ?>
         </form>
